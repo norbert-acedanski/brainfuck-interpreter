88ddadd256dcbc602034bea63a95beeb0ba4e854
Norbert Acedanski <norbert.acedanski@gmail.com>
2021-12-27 18:15:45 +0100

    Add printing functions and initial bytearray

--- brainfuckInterpreter.py
+++ brainfuckInterpreter.py
@@ -1,6 +1,9 @@
+printOriginalBrainFuckProgram = True
+printInstructionsWithouComment = True
 
 programName = "brainFuckProgram.txt"
 brainFuckCharacters = ['<', '>', '+', '-', '.', ',', '[', ']']
+brainFuckArray = bytearray(30000)
 
 def loadProgram():
     with open(programName, "r", encoding="utf-8") as inputFile:
@@ -8,12 +11,29 @@ def loadProgram():
     joinedProgram = "".join(roughProgram)
     return joinedProgram
 
+def printOriginalProgram(program):
+    if printOriginalBrainFuckProgram:
+        print("Original program from the specified file:\n")
+        print(program)
+        print("")
+
 def removeRedundantCharacters(program):
     onlyInstructionList = [character for character in program if character in brainFuckCharacters]
     onlyInstructionProgram = "".join(onlyInstructionList)
     return onlyInstructionProgram
 
+def printOnlyInstructions(program):
+    if printInstructionsWithouComment:
+        print("Instructions from the specified file:\n")
+        print(program)
+        print("")
+
+def interpretProgram(program):
+    pass
+
 if __name__ == '__main__':
     roughProgram = loadProgram()
+    printOriginalProgram(roughProgram)
     onlyInstructionProgram = removeRedundantCharacters(roughProgram)
-    print(onlyInstructionProgram)
+    printOnlyInstructions(onlyInstructionProgram)
+    interpretProgram(onlyInstructionProgram)
