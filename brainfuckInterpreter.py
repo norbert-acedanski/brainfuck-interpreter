3f0785c28ede0a84f287dba534a252d81009e94f
Norbert Acedanski <norbert.acedanski@gmail.com>
2021-12-27 18:36:15 +0100

    Implement '<' and '>' instructions

--- brainfuckInterpreter.py
+++ brainfuckInterpreter.py
@@ -1,8 +1,11 @@
+import sys
+
 printOriginalBrainFuckProgram = True
 printInstructionsWithouComment = True
 
 programName = "brainFuckProgram.txt"
 brainFuckCharacters = ['<', '>', '+', '-', '.', ',', '[', ']']
+ARRAY_SIZE = 30000
 brainFuckArray = bytearray(30000)
 
 def loadProgram():
@@ -29,7 +32,27 @@ def printOnlyInstructions(program):
         print("")
 
 def interpretProgram(program):
-    pass
+    byteArrayIndex = 0
+    for instruction in program:
+        if   instruction == '<':
+            byteArrayIndex = byteArrayIndex - 1 if byteArrayIndex > 0 else ARRAY_SIZE - 1
+        elif instruction == '>':
+            byteArrayIndex = byteArrayIndex + 1 if byteArrayIndex < ARRAY_SIZE - 1 else 0
+        elif instruction == '+':
+            pass
+        elif instruction == '-':
+            pass
+        elif instruction == '.':
+            pass
+        elif instruction == ',':
+            pass
+        elif instruction == '[':
+            pass
+        elif instruction == ']':
+            pass
+        else:
+            print("Wrong input instruction at: " + str(byteArrayIndex))
+            sys.exit()
 
 if __name__ == '__main__':
     roughProgram = loadProgram()
