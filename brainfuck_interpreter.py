c21e8fdcdfa3816b2b8566383bbfed6667992f8e
Norbert Acedanski <norbert.acedanski@gmail.com>
2022-11-30 21:55:57 +0100

    Refactor main file.

--- brainfuck_interpreter.py
+++ brainfuck_interpreter.py
@@ -6,14 +6,16 @@ BRAINFUCK_CHARACTERS = ['<', '>', '+', '-', '.', ',', '[', ']']
 ARRAY_SIZE = 30000
 brainfuck_array = bytearray(ARRAY_SIZE)
 
+
 def load_program() -> str:
     try:
         with open(program_filename, "r", encoding="utf-8") as input_file:
             rough_program = input_file.readlines()
     except OSError:
-        raise FileNotFoundError("File \"" + program_filename + "\" could not be opened. Check file name or file path.")
+        raise FileNotFoundError(f'File "{program_filename}" could not be opened. Check file name or file path.')
     joined_program = "".join(rough_program)
     return joined_program
+
 
 def print_original_program(program: str):
     if print_original_brainfuck_program:
@@ -21,15 +23,18 @@ def print_original_program(program: str):
         print(program)
         print("")
 
+
 def remove_redundant_characters(program: str) -> str:
     only_instruction_program = "".join([character for character in program if character in BRAINFUCK_CHARACTERS])
     return only_instruction_program
+
 
 def print_only_instructions(program: str):
     if print_instructions_without_comments:
         print("\nInstructions from the specified file:\n")
         print(program)
         print("")
+
 
 def make_bracket_list(program: str) -> list:
     bracket_list = [[], []] 
@@ -43,7 +48,8 @@ def make_bracket_list(program: str) -> list:
         elif instruction == ']':
             number_of_closing_brackets += 1
             if number_of_closing_brackets > number_of_opening_brackets:
-                raise SyntaxError("Mistake in the program. Found closing bracket without corresponding opening bracket in position: " + str(index))
+                raise SyntaxError(f"Mistake in the program. Found closing bracket without corresponding "
+                                  f"opening bracket in position: {index}")
             for i in range(number_of_opening_brackets, 0, -1):
                 if bracket_list[1][i - 1] == 0:
                     bracket_list[1][i - 1] = index
@@ -51,8 +57,10 @@ def make_bracket_list(program: str) -> list:
     if number_of_opening_brackets > number_of_closing_brackets:
         for i in range(number_of_opening_brackets):
             if bracket_list[1][i] == 0:
-                raise SyntaxError("Mistake in the program. Found opening bracket without corresponding closing bracket in position: " + str(bracket_list[0][i]))
+                raise SyntaxError(f"Mistake in the program. Found opening bracket without corresponding "
+                                  f"closing bracket in position: {bracket_list[0][i]}")
     return bracket_list
+
 
 def interpret_program(program: str, bracket_list: list):
     print("\nInterpreted program:\n")
@@ -65,9 +73,11 @@ def interpret_program(program: str, bracket_list: list):
         elif instruction == '>':
             byte_array_index = byte_array_index + 1 if byte_array_index < ARRAY_SIZE - 1 else 0
         elif instruction == '+':
-            brainfuck_array[byte_array_index] = brainfuck_array[byte_array_index] + 1 if brainfuck_array[byte_array_index] < 255 else 0
+            brainfuck_array[byte_array_index] = brainfuck_array[byte_array_index] + 1 \
+                if brainfuck_array[byte_array_index] < 255 else 0
         elif instruction == '-':
-            brainfuck_array[byte_array_index] = brainfuck_array[byte_array_index] - 1 if brainfuck_array[byte_array_index] > 0 else 255
+            brainfuck_array[byte_array_index] = brainfuck_array[byte_array_index] - 1 \
+                if brainfuck_array[byte_array_index] > 0 else 255
         elif instruction == '.':
             print(chr(brainfuck_array[byte_array_index]), end="")
         elif instruction == ',':
@@ -80,8 +90,9 @@ def interpret_program(program: str, bracket_list: list):
             instruction_index = bracket_list[0][bracket_list[1].index(instruction_index)]
             continue
         else:
-            raise SyntaxError("Wrong input instruction at: " + str(byte_array_index))
+            raise SyntaxError(f"Wrong input instruction at: {byte_array_index}")
         instruction_index += 1
+
 
 if __name__ == '__main__':
     rough_program = load_program()
